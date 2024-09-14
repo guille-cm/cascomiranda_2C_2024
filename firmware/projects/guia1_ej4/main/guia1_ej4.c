cb1e2ff853d2222a28d33ff450c1e8806abba709
guille-cm <guillermo_casco_miranda@hotmail.com>
2024-09-13 21:49:53 -0300

    Guía 1, ejercicio 4 - Converción de decimal a BCD

--- firmware/projects/guia1_ej4/main/guia1_ej4.c
+++ firmware/projects/guia1_ej4/main/guia1_ej4.c
@@ -1,28 +1,11 @@
-/*! @mainpage Template
+/*! @mainpage Conversion de un numero decimal a BCD
  *
- * @section genDesc General Description
- *
- * This section describes how the program works.
- *
- * <a href="https://drive.google.com/...">Operation Example</a>
- *
- * @section hardConn Hardware Connection
- *
- * |    Peripheral  |   ESP32   	|
- * |:--------------:|:--------------|
- * | 	PIN_X	 	| 	GPIO_X		|
+ * \section genDesc General Description
  *
  *
- * @section changelog Changelog
- *
- * |   Date	    | Description                                    |
- * |:----------:|:-----------------------------------------------|
- * | 12/09/2023 | Document creation		                         |
- *
- * @author Albano Peñalva (albano.penalva@uner.edu.ar)
+ * @author Guillermo Casco Miranda guillermo.casco.miranda@gmail.com
  *
  */
-
 /*==================[inclusions]=============================================*/
 #include <stdio.h>
 #include <stdint.h>
@@ -32,8 +15,43 @@
 
 /*==================[internal functions declaration]=========================*/
 
+/** @fn int8_t BinaryToBcd (uint32_t data, uint8_t digits, uint8_t *bcd_number)
+ * @brief Conversion de un decimal de 32 bit, con n digitos,
+ * a un arreglo de enteros de 8 bits, en bcd
+ * @param[in] decimal número, número de dígitos, puntero pasado por referencia
+ * @return return FALSE if no error
+ */
+int8_t BinaryToBcd (uint32_t data, uint8_t digits, uint8_t *bcd_number){
+	unsigned int i;
+	for(i=digits;i>0;i--){
+		bcd_number[i]=data%10;
+		data=data/10;
+	}
+	return 0;
+}
+
 /*==================[external functions definition]==========================*/
+
+/**
+ * @brief Punto de entrada del programa.
+ * 
+ * El programa covierte un decimal a BCD
+ * 
+ * @return void
+ */
 void app_main(void){
-	printf("Hello world!\n");
+	/* initializations */
+	
+	uint32_t datos=1234;
+	uint8_t digitos=4;
+	uint8_t bcd[digitos];
+
+
+	
+    while(1){
+		/* main loop */
+    	BinaryToBcd(datos,digitos,bcd);
+    	for(int i=0;i<50000;i++){};
+	}
 }
 /*==================[end of file]============================================*/
